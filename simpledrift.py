b564a1e824c9db66a22611d0ad39fcef3a506c68
Ryan Maloney <ryan.t.maloney@gmail.com>
2020-10-22 17:57:11 -0400

    Lots of graphing changing

--- simpledrift.py
+++ simpledrift.py
@@ -15,28 +15,39 @@ def driftmodeling(flynum, numberofbins, numberofdays, prefmean, prefvariance, en
     envi=np.zeros((numberofbins,numberofdays))
     envi[:,0]=sci.norm.pdf(x,envimean,envivariance) # A gaussian of environment with center around 0
     # print(np.max(envi))
-    envi=envi/(np.max(envi))*deathrate
+    envi[:,0]=envi[:,0]/(np.max(envi[:,0]))*deathrate
     for t in range(1,numberofdays):
         envi[:,t]=sci.norm.pdf(x,(envimean+gain*np.sin(t*np.pi*2/per)),envivariance)
-        envi[:,t]=envi[:,t]/np.max(envi[:,t])*.95
+        envi[:,t]=envi[:,t]/np.max(envi[:,t])*deathrate
         for b in range(numberofbins):
             pref[:,t]+=pref[b,t-1]*sci.norm.pdf(x,x[b],driftvariance)/np.sum(sci.norm.pdf(x,x[b],driftvariance))
             # print(np.sum(sci.norm.pdf(x,x[b],driftvariance)))
         # plt.plot(pref[:,t])
         # plt.show()
-        pref[:,t]=np.multiply(pref[:,t], envi[:,t]) # Multiplying the preference to the environment
         # print(pref[:,t])
-        pref[:,t]=pref[:,t]+pref[:,0]*birthrate/flynum*np.sum(pref[:,t-1])
+        pref[:,t]=pref[:,t]+pref[:,0]*birthrate/flynum*np.sum(pref[:,t])
+        pref[:,t]=np.multiply(pref[:,t], envi[:,t]) # Multiplying the preference to the environment
+    fig, (ax0, ax1, ax2) = plt.subplots(3, 1)
+    fig.set_figwidth(8)
+    fig.set_figheight(8)
 
-    plt.pcolormesh(pref)
-    plt.colorbar()
-    plt.title('Fly Preference (color is num flies each day')
-    plt.ylabel('Preference')
-    plt.xlabel('Day')
-    plt.show()
-    plt.pcolormesh(envi)
-    plt.colorbar()
-    plt.title('Environment (color is fraction of flies of given pref die)')
-    plt.ylabel('Preference')
-    plt.xlabel('Day')
+    fig.tight_layout()
+    plt.subplots_adjust(hspace=.4)
+    c=ax0.pcolormesh(envi)
+    fig.colorbar(c,ax=ax0)
+    ax0.set_title('Environment (color is fraction of flies of given pref die)')
+    ax0.set_ylabel('Preference')
+    ax0.set_xlabel('Day')
+
+    c=ax1.pcolormesh(np.log(pref))
+    fig.colorbar(c,ax=ax1)
+    ax1.set_title('Fly Preference (color is log(num) flies each day)')
+    ax1.set_ylabel('Preference')
+    ax1.set_xlabel('Day')
+
+    ax2.plot(np.log(np.sum(pref,0)))
+    ax2.set_title('total log(num) flies)')
+    ax2.set_ylabel('log(num) flies)')
+    ax2.set_xlabel('Day')
+
     plt.show()
