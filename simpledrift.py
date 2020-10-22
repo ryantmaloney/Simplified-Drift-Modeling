1e0417b2f927d98570f85832543daa10dc59b656
Ryan Maloney <ryan.t.maloney@gmail.com>
2020-10-22 16:30:31 -0400

    Few more additions to control birthrate and deathrate and fix bugs

--- simpledrift.py
+++ simpledrift.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.stats as sci
 import matplotlib.pyplot as plt
 
-def driftmodeling(flynum, numberofbins, numberofdays, prefmean, prefvariance, envimean, envivariance, driftvariance, gain, per):
+def driftmodeling(flynum, numberofbins, numberofdays, prefmean, prefvariance, envimean, envivariance, driftvariance, gain, per, deathrate, birthrate):
     x=np.linspace(-1,1,numberofbins)
     pref=np.zeros((numberofbins,numberofdays))
     pref[:,0]=sci.norm.pdf(x,prefmean,prefvariance) # A gaussian of preference with center around 0
@@ -15,7 +15,7 @@ def driftmodeling(flynum, numberofbins, numberofdays, prefmean, prefvariance, en
     envi=np.zeros((numberofbins,numberofdays))
     envi[:,0]=sci.norm.pdf(x,envimean,envivariance) # A gaussian of environment with center around 0
     # print(np.max(envi))
-    envi=envi/(np.max(envi))*.95
+    envi=envi/(np.max(envi))*deathrate
     for t in range(1,numberofdays):
         envi[:,t]=sci.norm.pdf(x,(envimean+gain*np.sin(t*np.pi*2/per)),envivariance)
         envi[:,t]=envi[:,t]/np.max(envi[:,t])*.95
@@ -24,13 +24,19 @@ def driftmodeling(flynum, numberofbins, numberofdays, prefmean, prefvariance, en
             # print(np.sum(sci.norm.pdf(x,x[b],driftvariance)))
         # plt.plot(pref[:,t])
         # plt.show()
-        pref[:,t]=np.multiply(pref[:,t], envi[:,0]) # Multiplying the preference to the environment
+        pref[:,t]=np.multiply(pref[:,t], envi[:,t]) # Multiplying the preference to the environment
         # print(pref[:,t])
-        pref[:,t]=pref[:,t]+pref[:,0]*.05/flynum*np.sum(pref[:,t-1])
+        pref[:,t]=pref[:,t]+pref[:,0]*birthrate/flynum*np.sum(pref[:,t-1])
 
     plt.pcolormesh(pref)
     plt.colorbar()
+    plt.title('Fly Preference (color is num flies each day')
+    plt.ylabel('Preference')
+    plt.xlabel('Day')
     plt.show()
     plt.pcolormesh(envi)
     plt.colorbar()
+    plt.title('Environment (color is fraction of flies of given pref die)')
+    plt.ylabel('Preference')
+    plt.xlabel('Day')
     plt.show()
