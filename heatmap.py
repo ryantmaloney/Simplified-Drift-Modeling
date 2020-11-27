95ad44cdd8cfb674e3121a68f9c0e570c52d9351
athena-ye <athenaye@college.harvard.edu>
2020-11-27 15:04:44 -0500

    Delete heatmap.py

--- heatmap.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import simpledrift as sd
-
-#[finalpop]=sd.driftmodeling(flynum, numberofbins, numberofdays, prefmean[q], prefvariance[q], envimean, envivariance, driftvariance[q], gain, per, deathrate, birthrate, matureage, percentbh)
-
-#sd.driftmodeling(flynum, numberofbins, numberofdays, prefmean, prefvariance, envimean, envivariance, driftvariance, gain, per,maxsurvivalrate,birthrate,matureage, percentbh)
-
